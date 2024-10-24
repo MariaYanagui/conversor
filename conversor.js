aea38ee871d717a8e5ffc098e9c3e9caa8aa66d9
Maria Yanagui <maria.yanagui@alunoescolassociais.org.br>
2024-10-24 11:52:26 -0300

    inicio do js

---
+++ conversor.js
@@ -0,0 +1,6 @@
+document.getElementById('currency-form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // OBTER VALORES DE ENTRADA INFORMADOS PELO USUÁRIO
+    const valor = parseFloat(document.getElementById('valor').value);
+});
